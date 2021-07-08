90d01992b5b9673ed106aa16246ee1af642f832f
Sneh17029 <60146980+Sneh17029@users.noreply.github.com>
2021-07-08 18:55:05 +0530

    Update 606. Construct String from Binary Tree.py

--- 606. Construct String from Binary Tree.py
+++ 606. Construct String from Binary Tree.py
@@ -1,9 +1,13 @@
+# Runtime: 36 ms, faster than 99.47% of Python3 online submissions for Construct String from Binary Tree.
+# Memory Usage: 17.1 MB, less than 5.76% of Python3 online submissions for Construct String from Binary Tree.
+
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
 class Solution:
     def tree2str(self, root: TreeNode) -> str:
         ans=[]
