5055e0ab1ddf84803d605fdb263fc70c14f0432f
Sneh17029 <60146980+Sneh17029@users.noreply.github.com>
2021-07-09 20:02:55 +0530

    Add files via upload

---
+++ Second Minimum Node In a Binary Tree.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def findSecondMinimumValue(self, root: TreeNode) -> int:
+        m1,m2=inf,inf
+        def h(node):
+            nonlocal m1,m2
+            if not node:
+                return 
+            m1=m1 if m1<node.val else node.val
+            if(m1!=node.val):
+                m2=m2 if m2<node.val else node.val
+            if(node.left):
+                h(node.left)
+            if(node.right):
+                h(node.right)
+            return m2 if m2!=inf else -1
+        return h(root)
